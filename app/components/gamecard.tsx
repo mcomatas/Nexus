8ba60f7f348bf610c3163a72e3278b4a42d14c5e
Michael Comatas <mcomatas99@gmail.com>
2024-07-17 17:24:35 -0400

    Added calls from IGDB API in the games page to display game covers.

--- app/components/gamecard.tsx
+++ app/components/gamecard.tsx
@@ -5,12 +5,12 @@ import Image from 'next/image'
 import styled from 'styled-components'
 
 const GameItem = styled(Image)`
-    border: solid 2px grey;
-    border-radius: 10px;
+    border: solid 2px black;
+    border-radius: 15px;
 `
 
 export const GameCard = ({src, alt}) => {
     return (
-        <GameItem src={src} alt={alt} width={250} height={275} />
+        <GameItem src={src} alt={alt} height={350} width={263} quality={100} />
     )
 }
--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,10 +1,81 @@
 import Link from 'next/link';
 import { GameCard } from '../components/gamecard'
 
-export default function Page() {
+const params = {
+    'client_id': process.env.IGDB_CLIENT_ID,
+    'client_secret': process.env.IGDB_CLIENT_SECRET,
+    'grant_type': 'client_credentials'
+}
+
+// Function to get bearer access token
+async function getAccessToken() {
+    try {
+        const token_response = await fetch("https://id.twitch.tv/oauth2/token", {
+            method: 'POST',
+            body: JSON.stringify(params),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+        
+        if (!token_response.ok) {
+            throw new Error(`Response status: ${token_response.status}`);
+        }
+
+        let data = await token_response.json();
+        return data.access_token;
+        
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+// Function to use the bearer access token and get a response from IGDB
+async function getData() {
+    const access_token = await getAccessToken();
+    try {
+        const response = await fetch("https://api.igdb.com/v4/covers", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Client-ID': params.client_id,
+                'Authorization': `Bearer ${access_token}`
+            },
+            body: "fields game,url,height,width; limit 10;"
+        })
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        return response.json();
+
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+function makeRow(games) {
+    const row = games.map((game) => 
+        <li key={game.id}><GameCard src={"https:" + game.url.replace("t_thumb", "t_cover_big")} alt="Game Cover"/></li>
+    )
+    return row;
+}
+
+export default async function Page() {
+    const data = await getData();
+    const data2d = [];
+    while (data.length) data2d.push(data.splice(0,4)); // Make a 2D array with rows fo length 4
+    const games = data2d.map((row, index) => 
+        <div className='main' key={index}>
+            {makeRow(row)}
+        </div>
+    )
+
     return (
         <div>
             <h1>Games page</h1>
+            {games}
         </div>
     )
 }
