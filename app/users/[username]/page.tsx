27747776d1550737bf2ee2b37d1b4a9f0f7fbfff
Michael Comatas <mcomatas99@gmail.com>
2025-06-01 16:49:42 -0400

    Updated user profile page to use useSWR to reduce code bloat and help with caching.

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,13 +1,13 @@
 'use client'
 
-import { use } from 'react';
 //import { SignOut } from '../../ui/form';
 import { signOut, SessionProvider } from 'next-auth/react';
 import { auth } from '../../../auth';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import { prisma } from '../../../prisma';
 import { GameCard } from '../../components/gamecard';
 import Pagination from '../../ui/pagination';
+import useSWR from 'swr';
 
 interface Props {
     params: {
@@ -17,57 +17,18 @@ interface Props {
 
 const PAGE_SIZE = 32;
 
-//`fields name, slug, cover.url; where id = (${ids.join(',')}); limit 20;`
-//const page = searchParams?.page || 1;
-//const offset = (page - 1) * PAGE_SIZE;
-//const bodyMain = `fields name, slug, cover.url; where cover != null & game_type = (0,8); limit ${PAGE_SIZE}; offset ${offset};`
-/*const body = query.length > 0 
-            ? `search "${query}"; ${bodyMain}`
-            : `${bodyMain} sort total_rating_count desc;`;*/
-
 export default function Page({ params }) {
-    //const { username } = await params;
-
-    //const page = searchParams?.page || 1;
-    //const offset = (page - 1) * PAGE_SIZE;
-    //const bodyMain = `fields name, slug, cover.url; where cover != null & game_type = (0,8); limit ${PAGE_SIZE}; offset ${offset};`
-
-    
+ 
     const obj = use(params);
-    //console.log(obj);
     const username = obj['username'];
-    //const email = obj['email'];
-    //console.log(username);
-    const [games, setGames] = useState([]);
-    const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        async function fetchGames() {
-            //const searchParams = await props.searchParams;
-            //const page = searchParams?.page || 1;
-            //const offset = (page - 1) * PAGE_SIZE;
-            //const body = `fields name, slug, cover.url; where cover != null & game_type = (0,8); limit ${PAGE_SIZE}; offset ${offset};`
+    const fetcher = url => fetch(url).then(r => r.json());
+    const { data, error, isLoading } = useSWR(`/api/users/${username}`, fetcher);
 
-            
-            const response = await fetch('/api/users/getGames', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    username: username,
-                })
-            });
-            const data = await response.json();
-            setGames(data['data'].games);
-            setCount(data['data'].count);
-        }
-        fetchGames();
-    }, [username]);
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error loading games.</p>;
 
-    if (!games || !count) return <p>Loading...</p>
-
-    const gamesArray = games.map((game) => 
+    const gamesArray = data.data.map((game) => 
         <div key={game.id}>
             <GameCard src={game.cover ? "https:" + game.cover.url.replace("t_thumb", "t_720p") : "/default-cover.webp"} alt={game.slug} slug={game.slug}/>
         </div>
