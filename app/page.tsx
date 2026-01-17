fd946f465b183476f3d96935573ae12ed229e60a
Michael Comatas <mcomatas99@gmail.com>
2026-01-17 17:08:08 -0500

    Added popscore APi request based on IGDB page visits. Added the top 10 games to the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,43 @@
-import Link from 'next/link';
-import { auth } from '../auth';
-import { prisma } from '../prisma'
+"use client";
 
-export default async function Page() {
-    const session = await auth();
+import Link from "next/link";
+//import { auth } from "../auth";
+import { prisma } from "../prisma";
+import { GameCard } from "./components/gamecard";
+import useSWR from "swr";
 
-    //console.log(session);
-    //console.log(session?.user?.email);
-    
-    return (
-        <div className="flex flex-col mx-auto p-5">
-            <h1 className="text-2xl">Welcome to the Nexus homepage!</h1>
-        </div>
-    )
+export default function Page() {
+  //const session = await auth();
+
+  //console.log(session);
+  //console.log(session?.user?.email);
+
+  const fetcher = (url) => fetch(url).then((r) => r.json());
+  const { data, error, isLoading } = useSWR(`/api/popscore`, fetcher);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading games</p>;
+
+  //console.log(data);
+
+  const gamesArray = data.map((game) => (
+    <div key={game.id}>
+      <GameCard
+        src={
+          game.cover
+            ? "https:" + game.cover.url.replace("t_thumb", "t_720p")
+            : "/default-cover.webp"
+        }
+        alt={game.slug}
+        slug={game.slug}
+      />
+    </div>
+  ));
+
+  return (
+    <div className="flex flex-col mx-auto p-5">
+      <h1 className="text-2xl">Welcome to the Nexus homepage!</h1>
+      {gamesArray}
+    </div>
+  );
 }
