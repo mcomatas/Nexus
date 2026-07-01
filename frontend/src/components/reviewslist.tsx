4470736dc4e91e51540ca9268da780df52db27b2
comatas <michael.comatas@nokia.com>
2026-07-01 10:09:08 -0400

    Update formatting / display of review list

--- frontend/src/components/reviewslist.tsx
+++ frontend/src/components/reviewslist.tsx
@@ -5,20 +5,19 @@ export const ReviewList = ({ reviews }: { reviews: Review[] }) => {
   const reviewMap = reviews.map((review) => (
     <div
       key={review.id}
-      className="flex flex-col gap-y-4 py-2"
+      className="flex flex-col gap-y-2 py-2"
     >
+      <div className="border-b border-text-muted"/>
       <div className="flex flex-row gap-x-1 text-text-muted">
         Review by <p className="font-semibold text-text-primary">{review.username} - {review.rating}</p>
       </div>
       <p className="text-text-secondary">{review.review_text}</p>
-      <div className="border-b border-text-muted py-2"/>
     </div>
   ))
 
   return (
-    <div className="w-full max-w-7/10 mx-auto p-2">
+    <div className="w-full max-w-7/10 mx-auto">
       <h1 className="font-bold">Reviews</h1>
-      <div className="border-b border-text-muted pt-1"/>
       {reviewMap}
     </div>
   )
