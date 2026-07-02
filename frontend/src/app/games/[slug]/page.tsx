db53686ae6eb76585818d17bae650fe8eef1df8f
Michael Comatas <mcomatas99@gmail.com>
2026-07-02 11:30:42 -0400

    Add pagination for reviews, increasing limit by 10 on each load more.

--- frontend/src/app/games/[slug]/page.tsx
+++ frontend/src/app/games/[slug]/page.tsx
@@ -3,27 +3,39 @@ import { GameDetail } from "@/components/gamedetail";
 import { ReviewsDetail } from "@/components/reviewsdetails";
 import { ReviewList } from "@/components/reviewslist";
 import { getCurrentUser } from "@/lib/session";
+import Link from "next/link";
 
 type GamePageProps = {
   params: Promise<{ slug: string }>;
+  searchParams: Promise<{ reviewLimit: string }>;
 }
 
-export default async function GamePage({ params }: GamePageProps) {
+export default async function GamePage({ params, searchParams }: GamePageProps) {
   const { slug } = await params
+  const { reviewLimit } = await searchParams;
+  const limit = Number(reviewLimit) || 10; // We'll make 10 default page size
   const path = `games/slug/${slug}`;
   const game = await apiFetch(path);
   const user = await getCurrentUser();
-  const reviewPath = `games/${game.igdb_id}/reviews${user ? `?userId=${user.id}` : ""}`;
+  const reviewPath = `games/${game.igdb_id}/reviews?limit=${limit}${user ? `&userId=${user.id}` : ""}`;
   const reviews = await apiFetch(reviewPath);
-  console.log(reviews);
 
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col mb-10">
         <GameDetail game={game}>
           <ReviewsDetail reviews={reviews} user={user} game={game} />
         </GameDetail>
         <ReviewList reviews={reviews.reviews} />
+        {reviews.reviews.length < Number(reviews.stats.review_count) && (
+          <Link
+            className="block w-fit my-6 bg-primary hover:bg-primary-dark transition-all cursor-pointer px-10 py-2 rounded-full mx-auto"
+            href={`/games/${slug}?reviewLimit=${limit + 10}`}
+            scroll={false}
+          >
+            Load More
+          </Link>
+        )}
       </div>
     </>
   );
--- frontend/src/components/reviewslist.tsx
+++ frontend/src/components/reviewslist.tsx
@@ -8,7 +8,9 @@ export const ReviewList = ({ reviews }: { reviews: Review[] }) => {
     >
       <div className="border-b border-text-muted"/>
       <div className="flex flex-row gap-x-1 text-text-muted">
-        Review by <p className="font-semibold text-text-primary">{review.username} - {review.rating}</p>
+        Review by
+        <p className="font-semibold text-text-primary">{review.username}</p>
+        {review.rating && <p className="text-text-secondary">- {review.rating}</p>}
       </div>
       <p className="text-text-secondary">{review.review_text}</p>
     </div>
@@ -16,8 +18,15 @@ export const ReviewList = ({ reviews }: { reviews: Review[] }) => {
 
   return (
     <div className="w-full max-w-7/10 mx-auto">
-      <h1 className="font-bold">Reviews</h1>
-      {reviewMap}
+      <h1 className="text-xl font-bold">Reviews</h1>
+      {reviews.length === 0 ?
+        <>
+          <div className="border-b border-text-muted py-1 mb-1"/>
+          <p className="text-text-secondary">No reivews yet...</p>
+        </>
+        :
+        reviewMap
+      }
     </div>
   )
 }
